a5d148c1d6d2236e32f8f7357f3a0c36be11ffbf
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2022-03-29 13:39:44 +0000

    feat: fix one attempt

--- flake.nix
+++ flake.nix
@@ -1,28 +1,24 @@
 {
-  description = "Configuration v1";
+	description = "Configuration v1";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
-    homeManager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+	inputs = {
+			nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+		
+			homeManager = {
+					url = "github:nix-community/home-manager";
+					inputs.nixpkgs.follows = "nixpkgs";
+				};
+		};
 
-  outputs = { self, nixpkgs, homeManager, ...}: {
-
-    nixosConfigurations."maxos" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        homeManager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.users.deepak = {
-            imports = [ ./home.nix ];
-          };
-        }
-      ];
-    };
-
-  };
+	outputs = { self, nixpkgs, homeManager, ...}@inputs: {
+		
+		nixosConfigurations = (
+			import ./hosts.nix {
+				inherit nixpkgs;
+				inherit homeManager;
+				inherit (nixpkgs) lib;
+			}
+		);
+		
+	};
 }
---
+++ hosts/hosts.nix
@@ -0,0 +1,28 @@
+{ lib, inputs, nixpkgs, homeManager, ... }:
+{
+	"maxos" = lib.nixosSystem {
+		system = "x86_64-linux";
+		modules = [
+			./maxos/configuration.nix
+			homeManager.nixosModules.home-manager {
+				home-manager.useGlobalPkgs = true;
+				home-manager.users.deepak = {
+					imports = [ ../home/deepak/home.nix ];
+				};
+			}
+		];
+	};
+
+	"nixosWSL" = lib.nixosSystem {
+		system = "x86_64-linux";
+		modules = [
+			./nixosWSL/configuration.nix
+			homeManager.nixosModules.home-manager {
+				home-manager.useGlobalPkgs = true;
+				home-manager.users.deepak = {
+					imports = [ ../home/deepak/home.nix ];
+				};
+			}
+		];
+	};
+}
