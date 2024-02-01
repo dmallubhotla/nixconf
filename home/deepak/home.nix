7e6d3687da55bb7407b6ac6fedb77eeee6a268f4
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2024-01-31 18:35:04 -0600

    update: updates, includes horrible hack for obsidian because EOL electron dependency

--- home/deepak/home.nix
+++ home/deepak/home.nix
@@ -1,4 +1,16 @@
-{ pkgs, config, specialArgs, ...}: {
+{ pkgs, config, specialArgs, lib, ...}:
+
+let
+  obsidian = lib.throwIf (lib.versionOlder "1.5.3" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
+    pkgs.obsidian.override {
+      electron = pkgs.electron_25.overrideAttrs (_: {
+        preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+      });
+    }
+  );
+in
+{
 
   programs.home-manager.enable = true;
   home.packages = [
@@ -17,7 +29,7 @@
     pkgs.nil
   ] ++ pkgs.lib.optionals specialArgs.withGUI [
     pkgs.discord
-    pkgs.obsidian
+    obsidian
     pkgs.nextcloud-client
   ];
 
