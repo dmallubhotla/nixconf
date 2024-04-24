61f9e25f53f949341e35892eb3d1703b140b5791
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2024-04-24 14:18:55 +0000

    vim puts lsp config in own file

--- home/deepak/home.nix
+++ home/deepak/home.nix
@@ -94,50 +94,12 @@
       lua << EOF
 
       vim.opt.tabstop = 4
+      vim.opt.shiftwidth = 4
       vim.opt.expandtab = false
       vim.opt.list = true
       vim.opt.listchars = { eol = "¬", tab = "▸┈" , trail = '·', multispace = '·' }
 
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
-
-      local cmp = require('cmp')
-      local cmp_format = lsp_zero.cmp_format({details = true})
-      local cmp_action = lsp_zero.cmp_action()
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        sources = {
-          {name = 'nvim_lsp'},
-          {name = 'buffer'},
-          {name = 'luasnip'},
-          {name = 'vimtex'},
-        },
-        formatting = cmp_format,
-        mapping = cmp.mapping.preset.insert({
-          -- ['<Tab>'] = cmp_action.tab_complete(),
-          -- ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-          ['<Tab>'] = cmp_action.luasnip_supertab(),
-          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-          ['<CR>'] = cmp.mapping.confirm({select = false}),
-        }),
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        }
-      })
-      require'lspconfig'.nil_ls.setup{}
-
+      ${builtins.readFile ./neovim/lsp.lua}
       vim.g.vim_markdown_folding_level = 2
       ${builtins.readFile ./neovim/wiki-vim.lua}
 
