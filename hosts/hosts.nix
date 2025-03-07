33823c8d2a91afd2ee3bb940bbb3a589b3f403d8
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2025-03-07 15:49:33 +0000

    updating nixosEggYoke to 2411

--- hosts/hosts.nix
+++ hosts/hosts.nix
@@ -89,7 +89,7 @@ in
 			NixOS-WSL-2405.nixosModules.wsl
 		];
 	};
-	"nixosEggYoke" = nixpkgs-24-05.lib.nixosSystem {
+	"nixosEggYoke" = inputs.nixpkgs-24-11.lib.nixosSystem {
 		system = "x86_64-linux";
 		specialArgs = {
 			inherit customPackageOverlay;
@@ -100,7 +100,7 @@ in
 		modules = [
 			./commonWSL-configuration.nix
 			inputs.sops-nix.nixosModules.sops
-			homeManager-24-05.nixosModules.home-manager {
+			inputs.homeManager-24-11.nixosModules.home-manager {
 				home-manager.extraSpecialArgs = {
 					withGUI = false;
 					gitSigningKey = "47831B15427F5A55";
@@ -114,7 +114,7 @@ in
 					inputs.sops-nix.homeManagerModules.sops
 				];
 			}
-			NixOS-WSL-2405.nixosModules.wsl
+			inputs.NixOS-WSL-2411.nixosModules.wsl
 		];
 	};
 }
