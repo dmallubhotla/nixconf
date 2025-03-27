2d4e53107d01cc60233d4e33a731240dca7b043e
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2025-03-27 16:35:32 -0500

    better oil keymaps to avoid conflict

--- home/deepak/neovim/init-vim.nix
+++ home/deepak/neovim/init-vim.nix
@@ -33,7 +33,31 @@
   }
 
   require('gitsigns').setup()
-  require("oil").setup()
+  require("oil").setup({
+    use_default_keymaps = false,
+    keymaps = {
+      ["g?"] = { "actions.show_help", mode = "n" },
+      ["<CR>"] = "actions.select",
+      ["<C-->"] = { "actions.select", opts = { vertical = true } },
+      ["<C-|>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-t>"] = { "actions.select", opts = { tab = true } },
+      ["<C-r>"] = "actions.preview",
+      ["<C-c>"] = { "actions.close", mode = "n" },
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = { "actions.parent", mode = "n" },
+      ["_"] = { "actions.open_cwd", mode = "n" },
+      ["`"] = { "actions.cd", mode = "n" },
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+      ["gs"] = { "actions.change_sort", mode = "n" },
+      ["gx"] = "actions.open_external",
+      ["g."] = { "actions.toggle_hidden", mode = "n" },
+      ["g\\"] = { "actions.toggle_trash", mode = "n" },
+    },
+    view_options = {
+      show_hidden = true,
+    },
+  })
+  vim.api.nvim_set_keymap("n", "<leader>oi", "<cmd>lua require('oil').open_float()<CR>", { noremap = true})
 
   -- require("nvim-web-devicons").setup({})
 
