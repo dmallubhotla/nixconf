3d878affcc1113b7d52fdb984a83049cc721770b
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2026-02-20 12:19:18 -0600

    nix: updaet inputs

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
     };
 
     claude-mcp-bundle = {
-      url = "git+ssh://git@gitea.deepak.science:2222/deepak/claude_mcp_bundle.git";
+      url = "git+ssh://git@github.com/dmallubhotla/claude_mcp_bundle.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
